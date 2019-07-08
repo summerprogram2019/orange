9897ad495e1d763cb825295c6e59d56bd1126aba
Gareth001 <2gareth2@gmail.com>
2019-07-08 18:35:27 +1000

    Changed default units, added initial sample data creation command

--- orange/food/models.py
+++ orange/food/models.py
@@ -8,21 +8,21 @@ class Nutrition(models.Model):
     calcium = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
     calories = models.DecimalField(max_digits=9, decimal_places=3, null=True) # Cal
     fat = models.DecimalField(max_digits=9, decimal_places=3, null=True) # g
-    sat_fat = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
+    sat_fat = models.DecimalField(max_digits=9, decimal_places=3, null=True) # g
     sodium = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
     carbs = models.DecimalField(max_digits=9, decimal_places=3, null=True) # g
-    fibre = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
+    fibre = models.DecimalField(max_digits=9, decimal_places=3, null=True) # g
     protein = models.DecimalField(max_digits=9, decimal_places=3, null=True) # g
-    vitamin_a = models.DecimalField(max_digits=9, decimal_places=3, null=True) # IU
+    vitamin_a = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
     vitamin_b12 = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
     vitamin_b6 = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
-    vitamin_c = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
-    vitamin_d = models.DecimalField(max_digits=9, decimal_places=3, null=True) # IU
+    vitamin_c = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
+    vitamin_d = models.DecimalField(max_digits=9, decimal_places=3, null=True) # ng
     vitamin_e = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
-    niacin = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
-    thiamin = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
+    niacin = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
+    thiamin = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
     phosphorus = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
-    zinc = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
+    zinc = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
     magnesium = models.DecimalField(max_digits=9, decimal_places=3, null=True) # mg
     folate = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
     riboflavin = models.DecimalField(max_digits=9, decimal_places=3, null=True) # µg
---
+++ orange/home/management/commands/createsampledata.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import Group
+from django.core.management import BaseCommand
+from django.db.utils import IntegrityError
+
+from food.models import Nutrition, Profile
+
+# creates some sample data to save time
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Creates some sample data"
+
+    def handle(self, *args, **options):
+        pass
+        
+        average_nutrition = Nutrition(
+            calories=2000,
+            calcium=1000,
+            fat=70,
+            protein=50,
+            sat_fat=24,
+            sodium=2300,
+            carbs=310,
+            fibre=30,
+            vitamin_a=600,
+            vitamin_b12=6,
+            vitamin_b6=2000,
+            vitamin_c=75,
+            vitamin_d=5000,
+            vitamin_e=10,
+            niacin=18,
+            thiamin=1400,
+            phosphorus=1000,
+            zinc=15,
+            magnesium=350,
+            folate=400,
+            riboflavin=1600,
+        )
+        average_nutrition.save()
+
+        # TODO create profile
