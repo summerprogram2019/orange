df79064c943d6322ebc9d7505a1214b19f69f8c0
Gareth001 <2gareth2@gmail.com>
2019-07-09 11:15:17 +1000

    Added other profiles, added scan link

--- orange/home/management/commands/createsampledata.py
+++ orange/home/management/commands/createsampledata.py
@@ -10,7 +10,6 @@ class Command(BaseCommand):
     help = "Creates some sample data"
 
     def handle(self, *args, **options):
-        pass
         
         average_nutrition = Nutrition(
             calories=2000,
@@ -37,4 +36,35 @@ class Command(BaseCommand):
         )
         average_nutrition.save()
 
-        # TODO create profile
+        average_profile = Profile(name="Average Nutrition", daily_intake=average_nutrition)
+        average_profile.save()
+
+        cataracts_nutrition = Nutrition(
+            calories=2000,
+            calcium=1000,
+            fat=70,
+            protein=50,
+            sat_fat=24,
+            sodium=2300,
+            carbs=310,
+            fibre=30,
+            vitamin_a=600,
+            vitamin_b12=6,
+            vitamin_b6=2000,
+            vitamin_c=75,
+            vitamin_d=5000,
+            vitamin_e=10,
+            niacin=18,
+            thiamin=1400,
+            phosphorus=1000,
+            zinc=15,
+            magnesium=350,
+            folate=400,
+            riboflavin=3000,
+        )
+        cataracts_nutrition.save()
+
+        cataracts_profile = Profile(name="Increased riboflavin for cataract prevention", 
+                daily_intake=cataracts_nutrition)
+        cataracts_profile.save()
+        
